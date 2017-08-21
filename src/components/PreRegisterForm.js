6d7bc26a371bb3ccd321cf1666a160c5d092b8f8
Roderik van der Veer <roderik@settlemint.com>
2017-08-21 16:54:35 +0200

    fix: unneeded action

--- src/components/PreRegisterForm.js
+++ src/components/PreRegisterForm.js
@@ -175,7 +175,6 @@ class PreRegisterForm extends Component {
               style={{ fontSize: '16px' }}
               readOnly
             />
-            <script>fbq('track', 'Lead');</script>
           </div>}
       </div>
     );
