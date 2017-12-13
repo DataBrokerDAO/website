a323cdb522c44359b8c7ac5a3cbfb347d81bc19d
Silke Van den Broeck <silkevandenbroeck@Silkes-MacBook-Pro.local>
2017-12-13 15:59:09 +0100

    fix: text and layout changes for pre main sale state

--- src/components/TokenSale.js
+++ src/components/TokenSale.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
-import { getWeb3 } from '../utils/getWeb3'
-import moment from 'moment'
-import ProgressBar from './ProgressBar'
+import React, { Component } from 'react';
+import { getWeb3 } from '../utils/getWeb3';
+import moment from 'moment';
+import ProgressBar from './ProgressBar';
 // import PreRegisterForm from './PreRegisterForm';
-import RegisterForm from './RegisterForm'
+import RegisterForm from './RegisterForm';
 
 class TokenSale extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       web3: null,
       sale: null,
@@ -19,8 +19,8 @@ class TokenSale extends Component {
       // MAX_ETH: 1,
       // MAX_ETH_OVER: 3,
       startFundingTime: moment.unix(1505746800),
-      endFundingTime: moment.unix(1508166000),
-    }
+      endFundingTime: moment.unix(1508166000)
+    };
   }
 
   componentWillMount() {
@@ -28,28 +28,28 @@ class TokenSale extends Component {
       getWeb3
         .then(results => {
           this.setState({
-            ...results,
-          })
+            ...results
+          });
           this.instantiateContract()
             .then(() => {
-              this.polling = true
-              this.longPoller()
+              this.polling = true;
+              this.longPoller();
             })
             .catch(error => {
-              console.log('longPoller error', error)
-            })
+              console.log('longPoller error', error);
+            });
         })
         .catch(e => {
-          console.log('Error finding web3.', e)
-        })
+          console.log('Error finding web3.', e);
+        });
     } else {
       this.setState({
         startFundingTime: moment.unix(1505746800),
         endFundingTime: moment.unix(1508166000),
         eth: 0,
         tokens: 0,
-        percentage: 0,
-      })
+        percentage: 0
+      });
     }
   }
 
@@ -57,93 +57,119 @@ class TokenSale extends Component {
     setTimeout(() => {
       this.instantiateContract(true).then(() => {
         if (this.polling) {
-          this.longPoller()
+          this.longPoller();
         }
-      })
-    }, 10000)
+      });
+    }, 10000);
   }
 
   componentWillUnmount() {
-    this.polling = false
+    this.polling = false;
   }
 
   async instantiateContract(update = false) {
     try {
-      const { sale, MAX_ETH, MAX_ETH_OVER } = this.state
-      const DeployedSale = await sale.deployed()
+      const { sale, MAX_ETH, MAX_ETH_OVER } = this.state;
+      const DeployedSale = await sale.deployed();
 
-      const totalCollected = await DeployedSale.totalCollected()
-      const eth = totalCollected.toNumber() / 10 ** 18
-      let totalSupply = MAX_ETH
+      const totalCollected = await DeployedSale.totalCollected();
+      const eth = totalCollected.toNumber() / 10 ** 18;
+      let totalSupply = MAX_ETH;
 
       if (eth >= MAX_ETH) {
-        totalSupply = MAX_ETH_OVER
+        totalSupply = MAX_ETH_OVER;
       }
 
       const newState = {
         eth,
         tokens: eth * 1200,
-        percentage: eth / totalSupply * 100,
-      }
+        percentage: eth / totalSupply * 100
+      };
       // console.log(newState);
-      this.setState(newState)
+      this.setState(newState);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
 
   saleUpcoming = () => {
     return (
       <div>
-        <h2 className="sale-date">Early Token Sale starts:</h2>
-        <p className="sale-date unmarg--bottom">18 SEPTEMBER 2017 - 5PM CET</p>
+        <h2 className="sale-date">
+          The early token sale is concluded. Thank you for your support!
+        </h2>
         <hr />
-        <h3 className="unmarg--bottom">
-          Register now to access the sale address before the start!
-        </h3>
-        <br />
-        <p className="lead">
-          You will also receive a unique referral link that earns you 5% of
-          every contribution you refer
+        <p className="sale-date unmarg--bottom">
+          The date of the main token sale will be announced shortly. You’ll hear
+          from us soon.
         </p>
-        <div className="modal-instance">
-          <a className="btn btn-lg type--uppercase btn--primary modal-trigger">
-            Register now for the early sale
-          </a>
-          <div style={{ marginTop: '15px' }}>
+        <hr />
+        <div style={{ marginTop: '15px' }}>
+          <p>
+            In the meanwhile we are still accepting larger volume purchases at
+            reduced rates.<br />{' '}
             <a
-              href="/how-to-participate.pdf"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fa fa-info-circle" aria-hidden="true" />
-            </a>{' '}
-            <a
-              href="/how-to-participate.pdf"
-              target="_blank"
-              rel="noopener noreferrer"
+              href="mailto:hello@databrokerdao.com"
               style={{ textDecoration: 'underline' }}
             >
-              How to participate in the early sale
+              Get in touch to discuss.
             </a>
-          </div>
-          <div className="modal-container">
-            <div className="modal-content">
-              <div className="boxed boxed--lg">
-                <RegisterForm upcoming={true} />
-              </div>
-            </div>
-          </div>
+          </p>
         </div>
       </div>
-    )
-  }
+    );
+    // OLD
+    // return (
+    //   <div>
+    //     <h2 className="sale-date">Early Token Sale starts:</h2>
+    //     <p className="sale-date unmarg--bottom">18 SEPTEMBER 2017 - 5PM CET</p>
+    //     <hr />
+    //     <h3 className="unmarg--bottom">
+    //       Register now to access the sale address before the start!
+    //     </h3>
+    //     <br />
+    //     <p className="lead">
+    //       You will also receive a unique referral link that earns you 5% of
+    //       every contribution you refer
+    //     </p>
+    //     <div className="modal-instance">
+    //       <a className="btn btn-lg type--uppercase btn--primary modal-trigger">
+    //         Register now for the early sale
+    //       </a>
+    //       <div style={{ marginTop: '15px' }}>
+    //         <a
+    //           href="/how-to-participate.pdf"
+    //           target="_blank"
+    //           rel="noopener noreferrer"
+    //         >
+    //           <i className="fa fa-info-circle" aria-hidden="true" />
+    //         </a>{' '}
+    //         <a
+    //           href="/how-to-participate.pdf"
+    //           target="_blank"
+    //           rel="noopener noreferrer"
+    //           style={{ textDecoration: 'underline' }}
+    //         >
+    //           How to participate in the early sale
+    //         </a>
+    //       </div>
+    //       <div className="modal-container">
+    //         <div className="modal-content">
+    //           <div className="boxed boxed--lg">
+    //             <RegisterForm upcoming={true} />
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // )
+  };
 
   saleRunning = doneLoading => {
-    const { endFundingTime, startFundingTime } = this.state
-    const togo = endFundingTime.diff(moment(), 'minutes')
-    const total = endFundingTime.diff(startFundingTime, 'minutes')
-    const percentage = 100 - (togo / total * 100)
+    const { endFundingTime, startFundingTime } = this.state;
+    const togo = endFundingTime.diff(moment(), 'minutes');
+    const total = endFundingTime.diff(startFundingTime, 'minutes');
+    const percentage = 100 - togo / total * 100;
     return (
       <div>
         <h2 className="sale-date padding-2" style={{ marginBottom: '1em' }}>
@@ -159,7 +185,10 @@ class TokenSale extends Component {
         )}
         {doneLoading && this.numberTable()}
         <div className="modal-instance">
-          <a className="btn btn-lg type--uppercase btn--primary modal-trigger" style={{fontWeight: 'bold', fontSize: '20px'}}>
+          <a
+            className="btn btn-lg type--uppercase btn--primary modal-trigger"
+            style={{ fontWeight: 'bold', fontSize: '20px' }}
+          >
             Buy your DATA tokens now!
           </a>
           <div style={{ marginTop: '15px' }}>
@@ -196,11 +225,11 @@ class TokenSale extends Component {
           </div>
         </div>
       </div>
-    )
-  }
+    );
+  };
 
   saleDone = () => {
-    const { percentage } = this.state
+    const { percentage } = this.state;
     return (
       <div>
         <h2 className="sale-date">The early token sale is concluded!</h2>
@@ -208,19 +237,20 @@ class TokenSale extends Component {
         <p className="sale-date unmarg--bottom">Thank you for your support!</p>
         <hr />
         <div style={{ marginTop: '15px' }}>
-        <a
-          href="mailto:hello@databrokerdao.com"
-          style={{ textDecoration: 'underline' }}
-        >
-          We are still accepting larger volume purchases at reduced rates.<br /> Get in touch to discuss.
-        </a>
+          <a
+            href="mailto:hello@databrokerdao.com"
+            style={{ textDecoration: 'underline' }}
+          >
+            We are still accepting larger volume purchases at reduced rates.<br />{' '}
+            Get in touch to discuss.
+          </a>
+        </div>
       </div>
-      </div>
-    )
-  }
+    );
+  };
 
   numberTable = () => {
-    const { tokens, eth, percentage, endFundingTime } = this.state
+    const { tokens, eth, percentage, endFundingTime } = this.state;
 
     return (
       <div>
@@ -233,32 +263,31 @@ class TokenSale extends Component {
                   {parseInt(tokens, 10).toFixed(0)}
                 </td>
               </tr>
-
             </tbody>
           </table>
         </div>
         <div className="col-sm-6 padding-only-left">
           <table>
             <tbody>
-            <tr>
-            <td style={{ textAlign: 'left' }}>ETH collected:</td>
-            <td style={{ textAlign: 'right' }} className="type--bold">
-              Ξ {parseInt(eth, 10).toFixed(0)}
-            </td>
-          </tr>
+              <tr>
+                <td style={{ textAlign: 'left' }}>ETH collected:</td>
+                <td style={{ textAlign: 'right' }} className="type--bold">
+                  Ξ {parseInt(eth, 10).toFixed(0)}
+                </td>
+              </tr>
             </tbody>
           </table>
         </div>
       </div>
-    )
-  }
+    );
+  };
 
   render() {
-    const { tokens, eth, percentage } = this.state
+    const { tokens, eth, percentage } = this.state;
 
-    let upcoming = process.env.REACT_APP_SALE_UPCOMING === 'true'
-    let done = process.env.REACT_APP_SALE_DONE === 'true'
-    let active = process.env.REACT_APP_SALE_ACTIVE === 'true'
+    let upcoming = process.env.REACT_APP_SALE_UPCOMING === 'true';
+    let done = process.env.REACT_APP_SALE_DONE === 'true';
+    let active = process.env.REACT_APP_SALE_ACTIVE === 'true';
 
     // if (startFundingTime && endFundingTime) {
     //   upcoming = startFundingTime.isAfter(moment());
@@ -267,9 +296,9 @@ class TokenSale extends Component {
     //   done = endFundingTime.isBefore(moment());
     // }
 
-    const doneLoading = percentage >= 0 && eth >= 0 && tokens >= 0
+    const doneLoading = percentage >= 0 && eth >= 0 && tokens >= 0;
 
-    setTimeout(() => window.modals(jQuery, window, document), 1000) //eslint-disable-line
+    setTimeout(() => window.modals(jQuery, window, document), 1000); //eslint-disable-line
 
     return (
       <div className="boxed boxed--lg border--round box-shadow-wide bg--white token-sale">
@@ -277,8 +306,8 @@ class TokenSale extends Component {
         {active && this.saleRunning(doneLoading)}
         {doneLoading && done && this.saleDone()}
       </div>
-    )
+    );
   }
 }
 
-export default TokenSale
+export default TokenSale;
