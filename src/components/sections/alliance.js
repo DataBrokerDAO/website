7cba323a200793754c8a8551782c94e8b3d94c20
Roderik van der Veer <roderik@settlemint.com>
2018-01-27 23:55:50 +0100

    fix: translations

--- src/components/sections/alliance.js
+++ src/components/sections/alliance.js
@@ -11,31 +11,11 @@ class AllianceSection extends PureComponent {
               <h2 style={{ marginBottom: '5px' }}>
                 <FormattedMessage id="alliance_title" />
               </h2>
-              <p
-                className="type--uppercase type--bold"
-                style={{ marginBottom: '20px' }}
-              >
-                The list is growing
-              </p>
               <p>
-                Building a platform and business like DataBroker DAO requires
-                real sellers and buyers of IoT sensor data. Over the past couple
-                of months we have been hard at work integrating these parties
-                into an alliance. A few months ago, we already announced the
-                first two official members of this DataBroker DAO alliance,
-                YUKTIX and SENTHUS. Today we are proud to announce DESIDEDATUM,
-                IQUADRAT, SKYLARK and TECHNILOG have joined.
+                <FormattedMessage id="alliance_intro" />
               </p>
-              <p>
-                <span style={{ fontStyle: 'italic' }}>
-                  "We are happy to cross paths with SettleMint and to engage
-                  with blockchain technology over DataBroker DAO. The
-                  integration was smooth and successful due to technical
-                  excellence. In a next step on our roadmap we intend to include
-                  a DataBrokerDAO “sell my data” option into our portfolio, as
-                  we realise this is a new monetisation opportunity for both
-                  sensor owners, data consumers and vendors"
-                </span>, said mr. Rajeev Jha - CEO of Yuktix
+              <p style={{ fontStyle: 'italic' }}>
+                <FormattedMessage id="alliance_quote" />
               </p>
             </div>
           </div>
@@ -43,58 +23,38 @@ class AllianceSection extends PureComponent {
             <div className="col-sm-1" />
             <div className="col-sm-2 modal-instance zooming-logo">
               <a className="modal-trigger" style={{ cursor: 'pointer' }}>
-                <img src="./img/logos alliance partners/Logo_BrusselsSmartCity.png" />
+                <img
+                  alt="Senthus"
+                  src="./img/logos alliance partners/Logo_BrusselsSmartCity.png"
+                />
               </a>
               <div className="modal-container">
                 <div className="modal-content">
                   <div className="boxed boxed--border">
                     <h2>Senthus</h2>
-                    <p>
-                      On the buyer side of things we find the SENTHUS consortium
-                      from our homeland Belgium. The SENTHUS consortium is,
-                      supported by the Brussels regions minister of economy,
-                      working to make the capital city of Brussels (both of the
-                      country and of the entire European Union) a smarter city.
-                    </p>
+                    <p />
                   </div>
                 </div>
               </div>
             </div>
             <div className="col-sm-2 modal-instance zooming-logo">
               <a className="modal-trigger" style={{ cursor: 'pointer' }}>
-                <img src="./img/logos alliance partners/logo_Yuktix.png" />
+                <img
+                  alt="Yuktix"
+                  src="./img/logos alliance partners/logo_Yuktix.png"
+                />
               </a>
               <div className="modal-container">
                 <div className="modal-content">
                   <div className="boxed boxed--border">
                     <h2>Yuktix</h2>
                     <p>
-                      YUKTIX is an IoT sensor manufacturer, based in Bangalore,
-                      India. The company creates next generation information
-                      systems for agriculture and environmental Monitoring.
-                      YUKTIX solutions can be used by farmers, growers,
-                      cooperatives and contractors or in Smart Cities to drive
-                      best practices, conserve resources and improve operation
-                      efficiency. YUKTIX provides a complete end-to- end
-                      solution. Meaning, just turn “ON” the sensor nodes and
-                      collect data on the YUKTIX SensorDB. Data distribution is
-                      also made possible via webhooks and REST API’s.
-                      <br />
-                      <br /> Which is how they tie in to the DataBroker DAO
-                      marketplace, offering their customers an easy path to
-                      recoup some or even all of the costs of their purchases.
+                      <FormattedMessage id="alliance_yuktix" />
                       <br />
                       <br />
                       <span style={{ fontStyle: 'italic' }}>
-                        "We are happy to cross paths with SettleMint and to
-                        engage with blockchain technology over DataBroker DAO.
-                        The integration was smooth and successful due to
-                        technical excellence. In a next step on our roadmap we
-                        intend to include a DataBrokerDAO “sell my data” option
-                        into our portfolio, as we realise this is a new
-                        monetisation opportunity for both sensor owners, data
-                        consumers and vendors"
-                      </span>, said mr. Rajeev Jha - CEO of Yuktix
+                        <FormattedMessage id="alliance_quote" />
+                      </span>
                     </p>
                   </div>
                 </div>
