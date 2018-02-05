7def0ebad1f0647ca5d44534e1e7ee8b549bde98
Hilke Heremans <hilke@thinman.be>
2018-02-05 11:23:56 +0100

    Modified for localhost

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,10 +1,14 @@
 import firebase from 'firebase';
 
 var config = {
-  apiKey: 'AIzaSyBoI_GMKEmv-UJaNeG_EfwpLLOjAMnGiSY',
-  authDomain: 'databrokerdao-presale.firebaseapp.com',
-  databaseURL: 'https://databrokerdao-presale.firebaseio.com',
-  storageBucket: 'databrokerdao-presale.appspot.com',
+  // apiKey: 'AIzaSyBoI_GMKEmv-UJaNeG_EfwpLLOjAMnGiSY',
+  // authDomain: 'databrokerdao-presale.firebaseapp.com',
+  // databaseURL: 'https://databrokerdao-presale.firebaseio.com',
+  // storageBucket: 'databrokerdao-presale.appspot.com',
+  apiKey: 'AIzaSyBsF57CHXhzZ0Z0LYCTy-LEQUUHX5ACxz8',
+  authDomain: 'databrokerdao-crowdsaledev.firebaseapp.com',
+  databaseURL: 'https://databrokerdao-crowdsaledev.firebaseio.com',
+  storageBucket: 'databrokerdao-crowdsaledev.appspot.com'
 };
 firebase.initializeApp(config);
 
