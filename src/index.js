57e57c86dcc9e5565c4b957886cca6945a696b07
Roderik van der Veer <roderik@settlemint.com>
2018-03-06 23:10:40 +0100

    workaround for safari in private mode

--- src/index.js
+++ src/index.js
@@ -17,6 +17,22 @@ const initialState = window.__INITIAL_STATE__;
 const history = createHistory();
 const store = createStore(initialState, history);
 
+// Safari, in Private Browsing Mode, looks like it supports localStorage but all calls to setItem
+// throw QuotaExceededError. We're going to detect this and just silently drop any calls to setItem
+// to avoid the entire page breaking, without having to do a check at each usage of Storage.
+if (typeof localStorage === 'object') {
+  try {
+    localStorage.setItem('localStorage', 1);
+    localStorage.removeItem('localStorage');
+  } catch (e) {
+    Storage.prototype._setItem = Storage.prototype.setItem;
+    Storage.prototype.setItem = function() {};
+    console.log(
+      'Your web browser does not support storing settings locally. In Safari, the most common cause of this is using "Private Browsing Mode". Some settings may not save or some features may not work properly for you.'
+    );
+  }
+}
+
 // Using this old school way, because even with a whatwg-url polyfill I couldn't get it to work on IE.
 const url = {};
 const location = window.location.href.split('#');
