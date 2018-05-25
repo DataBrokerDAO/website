45ec4dfa65f31feff56895384f0dd7f5614c90d7
Alon <alonp@rewire.to>
2018-05-25 12:38:58 +0300

    pref: dynamic import contract files

--- src/utils/getWeb3.js
+++ src/utils/getWeb3.js
@@ -1,12 +1,12 @@
-import Web3 from 'web3'
-import contract from 'truffle-contract'
-import saleJSON from '../contracts/TokenSale.json'
+export const getWeb3 = new Promise(async function(resolve, reject) {
+  const [Web3, contract, saleJSON] = await Promise.all([
+    import(/* webpackChunkName: 'contract' */ 'web3'),
+    import(/* webpackChunkName: 'contract' */ 'truffle-contract'),
+    import(/* webpackChunkName: 'contract' */ '../contracts/TokenSale.json'),
+  ]).catch(reject)
 
-export const getWeb3 = new Promise(function(resolve, reject) {
   // Wait for loading completion to avoid race conditions with web3 injection timing.
   //window.addEventListener('load', function() {
-  var results
-  var web3 = window.web3
 
   // Checking if Web3 has been injected by the browser (Mist/MetaMask)
   // if (typeof web3 !== 'undefined') {
@@ -22,16 +22,16 @@ export const getWeb3 = new Promise(function(resolve, reject) {
   //   resolve(dresults);
   // } else {
   //   // Fallback to localhost if no web3 injection.
-  var provider = new Web3.providers.HttpProvider(
-    process.env.REACT_APP_SALE_NETWORK
+  const provider = new Web3.providers.HttpProvider(
+    process.env.REACT_APP_SALE_NETWORK,
   )
 
-  web3 = new Web3(provider)
+  const web3 = new Web3(provider)
 
   const sale = contract(saleJSON)
   sale.setProvider(provider)
 
-  results = {
+  const results = {
     web3,
     sale,
   }
