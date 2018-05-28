fd0bbb828c6bce6c5f136a93032d3d055c35cb89
Alon <alonp@rewire.to>
2018-05-28 18:53:27 +0300

    pref: critical css tool

---
+++ scripts/critical-css.js
@@ -0,0 +1,14 @@
+const critical = require('critical')
+const glob = require("glob")
+
+const css = glob.sync('./build/static/css/*.css')
+
+critical.generate({
+  inline: true,
+  base: './build',
+  src: 'index.html',
+  dest: 'index.html',
+  css,
+  width: 1300,
+  height: 900
+});
